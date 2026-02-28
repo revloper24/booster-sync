be46816d570cc54260dad29ba8e4e005a404598f
revloper24 <ephesianpw@gmail.com>
2026-02-28 23:35:41 +0800

    Implement guild member update logging for boosts
    Added event listener for guild member updates to log boost actions.

--- index.js
+++ index.js
@@ -11,14 +11,31 @@ const client = new Client({
     ]
 });
 
+const GUILD_ID = process.env.GUILD_ID;
+const BOOSTER_ROLE_ID = process.env.BOOSTER_ROLE_ID;
+
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("guildMemberUpdate", (oldMember, newMember) => {
+    if (newMember.guild.id !== GUILD_ID) return;
+
+    const hadRole = oldMember.roles.cache.has(BOOSTER_ROLE_ID);
+    const hasRole = newMember.roles.cache.has(BOOSTER_ROLE_ID);
+
+    if (!hadRole && hasRole) {
+        console.log(`${newMember.user.tag} just boosted!`);
+    }
+
+    if (hadRole && !hasRole) {
+        console.log(`${newMember.user.tag} removed boost.`);
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
 const app = express();
-
 app.get("/", (_, res) => {
     res.send("Booster bot running");
 });
